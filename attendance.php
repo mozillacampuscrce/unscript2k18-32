1d5b75fd521b4a6609f71173d6afccdc8880f35d
Sagar Pathare <sspathare97@gmail.com>
2018-03-25 00:12:26 +0530

    Attendance, Background, Welcome

---
+++ attendance.php
@@ -0,0 +1,140 @@
+<?php
+  include('session.php');
+	if($_SERVER['REQUEST_METHOD'] != "POST")
+    header('location:welcome.php');
+  else{
+    $action = mysqli_real_escape_string($db,$_POST['action']);
+    $faculty = $_SESSION['login_faculty'];
+    $subject = mysqli_real_escape_string($db,$_POST['subject']);
+    $year = mysqli_real_escape_string($db,$_POST['year']);
+    $division = mysqli_real_escape_string($db,$_POST['division']);
+    if($action == "atform"){
+  		//subject details sent from form
+      date_default_timezone_set('Asia/Kolkata');
+      $date = date('Y-m-d');
+      $time = date('H:i');
+      $class = $year.'_'.$division;
+      $_SESSION['class']=$class;
+      $sql = "SELECT `Strength` FROM `Strength` WHERE  `Year` = '$year' and `Division` = '$division';";
+      if($result=mysqli_query($db,$sql)){
+        $row=mysqli_fetch_assoc($result);
+        //get strength from table
+        $strength = $row['Strength'];
+        $_SESSION['strength'] = $strength;
+      }
+    }
+    else{
+      $topics = mysqli_real_escape_string($db,$_POST['topics']);
+      $date = mysqli_real_escape_string($db,$_POST['date']);
+      $time = mysqli_real_escape_string($db,$_POST['time']);
+      $type = mysqli_real_escape_string($db,$_POST['type']);
+      $rno = mysqli_real_escape_string($db,$_POST['rno']);
+      $strength = $_SESSION['strength'];
+      //attendance array
+      $att=array_fill(0,$strength,1);
+      //rollnumber array from form
+      $noarray = preg_replace_callback('/(\d+)-(\d+)/', 
+        function($m) {
+          return implode(',', range($m[1], $m[2]));
+        }, $rno);
+      $noarray = explode(',', $noarray);
+      foreach (range(1, $strength) as $no) {
+        //in absent list
+        if(in_array($no, $noarray) && $type=="Absent Roll Numbers")
+          $att[$no -1]=0;
+        //not in present list
+        else if(!in_array($no, $noarray) && $type=="Present Roll Numbers")
+          $att[$no -1]=0;
+      }
+      $class = $_SESSION['class'];
+      $sql = "INSERT INTO `".$class."` VALUES ('$faculty','$subject','$topics','$date','$time'";
+      for($i = 1; $i<=$strength;$i++)
+        $sql = $sql.", ".$att[$i -1];
+      $sql = $sql.")";
+      if($result=mysqli_query($db,$sql))
+        header('location:welcome.php');
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h4><a href="welcome.php" class="btn btn-info" role="button">Back</a></h4>
+  <h2>Subject Attendance</h2>
+  <form class="form-horizontal" method="post" action="attendance.php">
+  <input type="hidden" name="action" value="attendance">
+  <div class="form-group">
+    <label class="control-label col-sm-2">Faculty:</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="faculty" value="<?php echo $faculty ?>" readonly>
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Subject:</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="subject" value="<?php echo $subject ?>" readonly>
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Year:</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="year" value="<?php echo $year ?>" readonly>
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Division:</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="division" value="<?php echo $division ?>" readonly>
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Topics covered:</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="topics" value="" placeholder="Enter topics covered during lecture">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Date:</label>
+    <div class="col-sm-5">
+      <input type="date" class="form-control" name="date" value="<?php echo $date ?>" placeholder="Enter date">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Time:</label>
+    <div class="col-sm-5">
+      <input type="time" class="form-control" name="time" value="<?php echo $time ?>" placeholder="Enter time">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Type:</label>
+    <div class="col-sm-5">
+    <select class="form-control" name="type">
+     <option>Absent Roll Numbers</option><option>Present Roll Numbers</option>
+    </select>
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2">Roll Numbers:</label>
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="rno" value="" placeholder="Enter roll numbers"><p class="help-block">Enter comma separated range or individaul numbers. eg. 12,23-26,45</p><h4 class="text-info">(Class Strength is <?php echo $_SESSION['strength']; ?>)</h4>
+    </div>
+    
+  </div>
+  <div class="form-group"> 
+    <div class="col-sm-offset-2 col-sm-5">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+</div>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,183 @@
+<?php 
+   include('session.php');
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		$fac = $_SESSION['login_faculty'];
+		$act = mysqli_real_escape_string($db,$_POST['action']);
+		$sub = mysqli_real_escape_string($db,$_POST['subject']);
+		$dv = mysqli_real_escape_string($db,$_POST['division']);
+		if($act == "remove"){
+			$sql = "DELETE FROM `Subject_Faculty` WHERE `Subject` = '$sub' and `Division` = '$dv' and `Faculty` = '$fac'";
+		}
+		else{
+			$temp = $sub;
+			$sub = substr($temp,5);
+			$yr = str_replace(" - ".$sub, "", $temp);
+			$sql = "INSERT INTO `Subject_Faculty` VALUES('$sub','$yr','$dv','$fac')";
+		}
+		$result = mysqli_query($db,$sql);
+		
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+	<h4 class="float">
+	<a href="index.php">View Attendance</a>
+	 </h4>
+	<h2>Welcome <?php echo $_SESSION['login_faculty']; ?></h2>
+	<h3>My Subjects</h3>
+	<?php
+		$sql = "SELECT * FROM `Subject_Faculty` WHERE `Faculty` = '".$_SESSION['login_faculty']."' ORDER BY FIELD(`Year`,'SE','TE','BE'), `Subject`";
+		if ($result=mysqli_query($db,$sql)){
+			echo "<table class=\"table table-hover table-bordered\">
+				<thead><tr>
+				<th>Subject</th> <th>Year</th> <th>Division</th> <th>Action</th>
+				</tr></theah><tbody>";
+			//No subjects
+			if(mysqli_num_rows($result)==0){
+				echo "<tr><td colspan=\"4\"><h4 class=\"text-danger\">No subjects to show!</h4></td></tr>";
+			}
+			else{
+				//Display subjects
+				while ($row=mysqli_fetch_assoc($result)){
+					echo "<tr>
+						<td>".$row['Subject']."</td>
+						<td>".$row['Year']."</td>
+						<td>".$row['Division']."</td> 
+						<td>
+						<form method=\"post\" action=\"attendance.php\">
+					  		<input type=\"hidden\" name=\"subject\" value=\"".$row['Subject']."\">
+					  		<input type=\"hidden\" name=\"year\" value=\"".$row['Year']."\">
+					  		<input type=\"hidden\" name=\"division\" value=\"".$row['Division']."\">
+					  		<input type=\"hidden\" name=\"action\" value=\"atform\">
+				        	<button type=\"submit\" class=\"btn btn-primary\">Attendance</button>
+							<button type=\"button\" class=\"btn btn-danger\" onclick=\"remModal('".$row['Subject']."','".$row['Year']."','".$row['Division']."')\">Remove Subject</button>
+						</form>
+						</td>
+						</tr>";
+				}
+			}
+			echo "</tbody><table>";
+			//Free result set
+			mysqli_free_result($result);
+		}
+	?>
+	<button type="button" class="btn btn-success" data-toggle="modal" data-target="#addCon">Add Subject</button>
+	<h3><a href = "logout.php">Sign Out</a></h3>
+    <h3><a href = "#deaCon" data-toggle="modal" >Deactivate</a></h3>
+</div>
+<!-- ModalRemove -->
+<div id="remCon" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Please Confirm Action!</h4>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to remove subject- '<span id="smod"></span> <span id="ymod"></span> <span id="dmod"></span>' from 'My Subjects'? This doesn't affect existing attendance records and you can add this subject again anytime.</p>
+      </div>
+      <div class="modal-footer">
+		<form method="post" action="welcome.php">
+	  		<input type="hidden" name="subject" value="" id="sub">
+	  		<input type="hidden" name="division" value="" id="dv">
+	  		<input type="hidden" name="action" value="remove">
+        	<button type="submit" class="btn btn-success">Confirm</button>
+		</form>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- ModalAdd -->
+<div id="addCon" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+	  <form method="post" action="welcome.php">
+  	  <input type="hidden" name="action" value="add">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Select Subject to add to 'My Subjects'</h4>
+      </div>
+      <div class="modal-body">
+      	<?php
+		$sql = "SELECT * FROM `Subject` ORDER BY FIELD(`Year`,'SE','TE','BE'), `Subject`";
+		if ($result=mysqli_query($db,$sql)){;
+			//No subjects
+			if(mysqli_num_rows($result)==0){
+				echo "<h4 class=\"text-danger\">No subjects to show!</h4>";
+			}
+			else{
+				//Display subjects
+				echo "<div class=\"row\"><div class=\"form-group col-xs-8\"><label>Year - Subject:</label><select class=\"form-control\" name=\"subject\">";
+				while ($row=mysqli_fetch_assoc($result)){
+					echo "<option>".$row['Year']." - ".$row['Subject']."</option>";
+				}
+				echo "</select></div>
+					<div class=\"form-group col-xs-4\"><label>Division:</label><select class=\"form-control\" name=\"division\">
+					<option>A</option><option>B</option>
+					</select></div></div>";
+			}
+			//Free result set
+			mysqli_free_result($result);
+		}
+	?>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-success">Add</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+      </div>
+      </form>
+    </div>
+
+  </div>
+</div>
+<!-- ModalDeactivate -->
+<div id="deaCon" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Please Confirm Action!</h4>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to deactivate your Faculty account? This cannot be undone!</p>
+      </div>
+      <div class="modal-footer">
+		<form method="post" action="deactivate.php">
+        	<button type="submit" class="btn btn-success">Confirm</button>
+		</form>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<script>
+	function remModal(subj,yr,dv){
+		document.getElementById("sub").value=subj;
+		document.getElementById("dv").value=dv;
+		document.getElementById("smod").innerHTML=subj;
+		document.getElementById("ymod").innerHTML=yr;
+		document.getElementById("dmod").innerHTML=dv;
+		$("#remCon").modal();
+		//alert(document.getElementById("sub").value);
+	}
+</script>
+</body>
+</html>
