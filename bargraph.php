74e98bf6db55721c6196a997dfb5bf1d81fa7662
Sagar Pathare <sspathare97@gmail.com>
2018-03-25 11:36:14 +0530

    Renamed Title

--- bargraph.php
+++ bargraph.php
@@ -7,7 +7,7 @@ $cat3 = $_GET['cat3'];
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Attendance and Marks Management</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="bargraph.css">
--- marks.php
+++ marks.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Attendance and Marks Management</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="style.css">
